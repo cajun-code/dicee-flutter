8d18a8db525a8c0e467a25f60b80c5873e5f961e
Allan Davis <cajun.code@gmail.com>
2019-05-22 23:11:03 -0400

    created dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
@@ -16,8 +16,43 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+@override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Die()
+          ),
+          Expanded(
+            child: Die()
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Die extends StatefulWidget {
+  @override
+  _DieState createState() => _DieState();
+}
+
+class _DieState extends State<Die> {
+  int diceValue = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FlatButton(
+      child: Image.asset('images/dice$diceValue.png'),
+      onPressed: (){
+        roll();
+      },
+    );
+  }
+
+  void roll(){
+    setState(() {
+        diceValue = Random().nextInt(6) + 1;
+      });
   }
 }
